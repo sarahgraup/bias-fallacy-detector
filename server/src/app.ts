fcc74ceef528b77a0e1d66fbb2fb140a0063ee21
sarahgraup <sarahgraup@g.ucla.edu>
2025-05-15 12:05:04 -0700

    added addiitional confidence modifiers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response, NextFunction } from "express";
-import router from "@/routes/api";
-import { BadRequestError, NotFoundError } from "@/expressError";
+import express, { Request, Response, NextFunction } from 'express';
+import router from '@/routes/api';
+import { BadRequestError, NotFoundError } from '@/expressError';
 
 const app = express();
 app.use(express.json());
-app.use("/apiRoutes", router);
+app.use('/apiRoutes', router);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   throw new NotFoundError();
 });
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  if (process.env.NODE_ENV !== 'test') console.error(err.stack);
 
   if (err instanceof NotFoundError || err instanceof BadRequestError) {
     const status = err.status || 500;
@@ -21,7 +21,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     });
   } else {
     const status = 500;
-    const message = "Internal Server Error";
+    const message = 'Internal Server Error';
     res.status(status).json({
       error: { message, status },
     });
